f46b68a0cc646d37606b166eb63697dd3582b69d
nikki <moniqshiin@gmail.com>
2025-02-17 17:22:19 +0800

    add log

--- Assets/Scripts/ShowSegment.cs
+++ Assets/Scripts/ShowSegment.cs
@@ -18,6 +18,7 @@ public class ShowSegment
     {
         float waitTime = KRGameManager.instance.showManager.waitTimeForRobotReset;
         Debug.Log($"開始第 {segmentIndex + 1} 個橋段, 等候{waitTime}秒等待手臂歸位...");
+        KRGameManager.instance.uiManager.AddLog($"開始第 {segmentIndex + 1} 個橋段, 等候{waitTime}秒等待手臂歸位...");
 
         KRGameManager.instance.animManager.animRobot = false;
         KRGameManager.instance.showManager.StartCoroutine(WaitForRobot(waitTime));
